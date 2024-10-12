fda91491595a1d1c68933c753cf132757ba374f5
OlivierLem <lemort.olivier.pro@gmail.com>
2024-10-12 15:38:21 +0200

    add experience section + data experience

--- server.ts
+++ server.ts
@@ -11,11 +11,14 @@ app.set("views", "./app/views");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-const { project } = JSON.parse(
+const { project, experience } = JSON.parse(
   readFileSync("app/database/Temp/data.json", "utf-8")
 );
 app.get("/", (req: Request, res: Response) => {
-  res.render(`./pages/homepage`, { list_project: project });
+  res.render(`./pages/homepage`, {
+    listProject: project,
+    listExperience: experience,
+  });
 });
 
 app.listen(port, () =>
